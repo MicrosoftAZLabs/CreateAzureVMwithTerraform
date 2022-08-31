93c1429a611390698ad5a40e489b6ee2398a421d
Mohit <mohit05054ece@gmail.com>
2022-08-31 23:20:47 +0530

    remove tag subnet

--- vnet-subnet.tf
+++ vnet-subnet.tf
@@ -12,6 +12,6 @@ resource "azurerm_subnet" "myTerraformSubnet" {
   virtual_network_name = azurerm_virtual_network.myTerraformNetwork.name
   address_prefixes     = ["10.0.1.0/24"]
 
-  tags= var.tags
+ 
 
   }
