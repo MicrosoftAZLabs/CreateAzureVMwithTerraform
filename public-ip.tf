c9f2f47cfd92b97012760dd400431d83fc7d8c80
Mohit <mohit05054ece@gmail.com>
2022-08-31 23:28:28 +0530

    adding IP address

--- public-ip.tf
+++ public-ip.tf
@@ -1,10 +1,8 @@
 resource "azurerm_public_ip" "myTerraformIP" {
   name                = var.ipaddressname
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  allocation_method   = "Static"
+  resource_group_name = azurerm_resource_group.myTerraformRG.name
+  location            = azurerm_resource_group.myTerraformRG.location
+  allocation_method   = "Dynamic"
 
-  tags = {
-    environment = "Production"
-  }
+  tags =var.tags
 }
