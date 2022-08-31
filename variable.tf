645ae60968e7a2885b5e0f62ac0f5de29226d046
Mohit <mohit05054ece@gmail.com>
2022-08-31 22:22:09 +0530

    update file

--- variable.tf
+++ variable.tf
@@ -1,9 +1,14 @@
 variable "location" {
     type= string
+    default = "East US"
 }
 variable "resourceGroupName" {
     type = string
+    default = "myTerraformRG"
 }
 variable "tags" {
     type= map(any)
+    default = {
+      "Env" = "Test"
+    }
 }
