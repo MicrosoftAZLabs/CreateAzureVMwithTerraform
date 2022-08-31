02155f2d214dea692d443f556a17a3d95a938468
Mohit <mohit05054ece@gmail.com>
2022-08-31 23:52:27 +0530

    adding storage account

--- storage-account.tf
+++ storage-account.tf
@@ -6,7 +6,7 @@ resource "random_id" "randomId" {
 }
 
 resource "azurerm_storage_account" "myTerraformStorage" {
-  name                = "diag${random_id.randomID.hex}"
+  name                = "diag${random_id.randomId.hex}"
   resource_group_name = azurerm_resource_group.myTerraformRG.name
   location            = azurerm_resource_group.myTerraformRG.location
   account_tier             = "Standard"
